b86755e04ad3f7161d56f056263fb40ee5083528
Chasel <arko9988b@gmail.com>
2023-12-03 20:03:25 +0800

    move files

---
+++ src/Provider/Providers.tsx
@@ -0,0 +1,12 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export function ToastProvider() {
+  return (
+    <ToastContainer
+      position='top-center'
+      autoClose={1500}
+      pauseOnFocusLoss={false}
+    />
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot }  from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import { ToastProvider } from './Provider/Providers.tsx'
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <CookiesProvider>
           <App />
-          <ToastContainer
-            position='top-center'
-            autoClose={1500}
-            pauseOnFocusLoss={false}
-          />
+          <ToastProvider/>
           <ReactQueryDevtools />
         </CookiesProvider>
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </StrictMode>,
 )
