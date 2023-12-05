99f7542786b62b2165ce8bec1be74cfc9357d87a
Chasel <arko9988b@gmail.com>
2023-12-05 22:28:36 +0800

    configuration eslint setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,33 @@
 module.exports = {
+  "settings": {
+    "react": {
+      "version": "detect",
+    },
+  },
   root: true,
   env: { browser: true, es2021: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended-type-checked",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-misused-promises": "warn",
+    "react/display-name": "off",
   },
 };
