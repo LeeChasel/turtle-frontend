f9df5fd6fed43f5c68703b1d30773e67e7451bb9
yez1125 <yez1125lin@gmail.com>
2024-02-25 18:39:37 +0800

    design

--- src/pages/CheckOrder.tsx
+++ src/pages/CheckOrder.tsx
@@ -14,7 +14,7 @@ function CheckOrder() {
     error,
   } = useOrderChecking(orderId!, email!, tokenCookie!);
 
-  function orderStatus(status: string) {
+  /*function orderStatus(status: string) {
     if (status === "SHIPPED") {
       return "運送中";
     } else if (status === "PAIED") {
@@ -28,7 +28,13 @@ function CheckOrder() {
     }
   }
 
-  const orderstatus = orderStatus(orderInfo!.orderStatus);
+  const orderstatus = orderStatus(orderInfo!.orderStatus);*/
+
+  /*function trace(){
+    if(orderInfo?.logisticsOrderStatus){
+
+    }
+  }*/
 
   if (status === "pending") {
     return <></>;
@@ -57,7 +63,7 @@ function CheckOrder() {
               <div>{orderInfo.orderDate}</div>
               <div>{orderInfo.shippingInfo.receiverName}</div>
               <div>{orderInfo.shippingInfo.receiverCellPhone}</div>
-              <div>{orderstatus}</div>
+              <div>{orderInfo.orderStatus}</div>
               <div>{}</div>
             </div>
 
@@ -75,11 +81,35 @@ function CheckOrder() {
                     <th className="w-[12%]">小計</th>
                   </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody>
+                  {orderInfo.item?.map((object) => (
+                    <tr key={object.productId}>
+                      <td className="break-all text-ellipsis">
+                        <span>{object.productName}</span>
+                      </td>
+                      <td className="break-all text-ellipsis">
+                        <span>{object.variationName}</span>
+                      </td>
+                      <td className="break-all text-ellipsis">
+                        {object.variationSpec}
+                      </td>
+                      <td>NT$ {object.currentPrice.toLocaleString()}</td>
+                      <td>{object.quantity.toLocaleString()}</td>
+                      <td>
+                        NT${" "}
+                        {(
+                          object.currentPrice * object.quantity
+                        ).toLocaleString()}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
               </table>
               <div className="flex justify-end mt-3">
                 <span>總金額：</span>
-                <span className="font-bold text-red-500">100</span>
+                <span className="font-bold text-red-500">
+                  {orderInfo.totalPrice}
+                </span>
               </div>
             </div>
           </div>
