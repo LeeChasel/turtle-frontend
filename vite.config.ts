f084ecb03d7599be76611cc4c03933cb9aa2ad42
Chasel <arko9988b@gmail.com>
2024-03-17 00:16:34 +0800

    remove useless dependencies and GA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
-import { VitePluginRadar } from "vite-plugin-radar";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -10,12 +9,6 @@ export default defineConfig({
     react(),
     checker({
       typescript: true,
-    }),
-    VitePluginRadar({
-      // Google Analytics tag injection
-      analytics: {
-        id: "G-SX2EH6Z0B5",
-      },
     }),
     tsconfigPaths(),
   ],
