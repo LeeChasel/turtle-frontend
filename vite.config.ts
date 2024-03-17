d852534e9ec8c38841ca584f021461120b88fd53
Chasel <arko9988b@gmail.com>
2024-03-17 15:29:49 +0800

    update vite config for ga

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePluginRadar } from "vite-plugin-radar";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-    }),
-    tsconfigPaths(),
-  ],
+// TODO: Need more flexibility to switch between development and production
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      checker({ typescript: true }),
+      tsconfigPaths(),
+      mode === "production" &&
+        VitePluginRadar({
+          analytics: {
+            id: "G-SX2EH6Z0B5",
+          },
+        }),
+    ],
+  };
 });
