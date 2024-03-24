fb7563dfb873e7c00c25863f8c51efc6ed76f942
Chasel <arko9988b@gmail.com>
2024-03-24 20:27:08 +0800

    Increase the readability of shipping information

--- src/pages/Merchant/OrderInfoDialog/ShippingInfo.tsx
+++ src/pages/Merchant/OrderInfoDialog/ShippingInfo.tsx
@@ -1,7 +1,7 @@
 import useUserTokenCookie from "@/hooks/useUserTokenCookie";
 import useSelectedOrder from "@/store/useSelectedOrder";
 import { OrderDetail } from "@/types/Order";
-import { transformOrderStatus } from "@/utils/transformStatusEnum";
+import { LogisticsSubType, LogisticsType } from "@/types/Shipping";
 import { useQueryClient } from "@tanstack/react-query";
 
 export default function ShippingInfo() {
@@ -14,19 +14,37 @@ export default function ShippingInfo() {
     orderId,
     tokenCookie,
   ]);
+
+  const hasLogisticsOrder = data?.logisticsOrderStatus.length !== 0;
+  if (!hasLogisticsOrder) {
+    return (
+      <div className="p-2 border border-black rounded bg-stone-50">
+        尚未出貨
+      </div>
+    );
+  }
+
   // 陣列最後的元素為最新的物流訂單資訊
-  const merchantShippingNo =
-    data?.logisticsOrderStatus.length === 0
-      ? "未出貨"
-      : data?.logisticsOrderStatus[data?.logisticsOrderStatus.length - 1]
-          .merchantTradeNo;
-  const shippingStatus = transformOrderStatus(data!.orderStatus);
+  const lastestLogisticsOrderStatus =
+    data!.logisticsOrderStatus[data!.logisticsOrderStatus.length - 1];
+
+  const shippingType = data!.shippingInfo.logisticsType!;
+  let shippingCode = "";
+  if (shippingType === LogisticsType.CVS) {
+    shippingCode = lastestLogisticsOrderStatus.cvspaymentNo;
+    if (data?.shippingInfo.logisticsSubType === LogisticsSubType.UNIMARTC2C) {
+      shippingCode += `${lastestLogisticsOrderStatus.cvsvalidationNo}`;
+    }
+  } else if (shippingType === LogisticsType.HOME) {
+    shippingCode = lastestLogisticsOrderStatus.bookingNote;
+  }
+
   return (
     <div className="p-2 border border-black rounded bg-stone-50">
-      <div>物流訂單編號：</div>
-      <div>{merchantShippingNo?.slice(-7)}</div>
+      <div>物流代碼/編號：</div>
+      <div>{shippingCode}</div>
       <div>物流狀態：</div>
-      <div>{shippingStatus}</div>
+      <div>{lastestLogisticsOrderStatus.rtnMsg}</div>
     </div>
   );
 }
