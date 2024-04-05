962ef56fe15988ac62a29ee3ff09094ee438bb1c
Chasel <arko9988b@gmail.com>
2024-04-05 17:11:25 +0800

    auto add extension to file in updateFile api

--- src/actions/updateFile.ts
+++ src/actions/updateFile.ts
@@ -1,10 +1,28 @@
-type JsonType = {
+import mimeTypes from "@/data/mimeTypes.json";
+import { findKey } from "lodash";
+
+type FileUpdateJson = {
   fileId: string;
+  mimeType?: string;
 };
 
 const URL = import.meta.env.VITE_TURTLE_PUBLIC_URL + "/file";
 
-async function updateFile(file: File): Promise<JsonType> {
+async function updateFile(
+  file: File,
+  identifyMimeType = false,
+): Promise<FileUpdateJson> {
+  const searchParams = new URLSearchParams();
+  const fileType = file.type;
+  // 取得副檔名(不含.)
+  const extension = findKey(mimeTypes, { mime: fileType });
+  if (!extension) {
+    console.error(fileType);
+    throw new Error("不支援的檔案格式，請確認上傳檔案類型或擴充支援的檔案格式");
+  }
+  searchParams.append("extension", extension);
+  searchParams.append("identifyMimeType", `${identifyMimeType}`);
+
   const form = new FormData();
   form.append("file", file);
   const res = await fetch(URL, {
@@ -16,7 +34,7 @@ async function updateFile(file: File): Promise<JsonType> {
     throw new Error("上傳圖片失敗");
   }
 
-  return res.json() as Promise<JsonType>;
+  return res.json() as Promise<FileUpdateJson>;
 }
 
 export default updateFile;
