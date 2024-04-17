e3de5e26894fdfb64f860220f25925ef14b0ea81
yez1125 <yez1125lin@gmail.com>
2024-04-18 02:41:57 +0800

    add region

--- src/pages/MusicTesting.tsx
+++ src/pages/MusicTesting.tsx
@@ -1,18 +1,22 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import WaveSurfer from "wavesurfer.js";
 import Hover from "wavesurfer.js/dist/plugins/hover.esm.js";
+import RegionsPlugin from "wavesurfer.js/dist/plugins/regions.esm.js";
 
 function MusicTesting() {
   const [file, setFile] = useState<File | null>(null);
   const containerRef = useRef(null);
-  const [fileURL, setFileURL] = useState<string | null>(null);
+  const [fileURL, setFileURL] = useState<string | null>(null); //改成陣列
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(8);
+  const [flag, setFlag] = useState(false);
 
   useEffect(() => {
     const container = containerRef.current;
-    if (!container) return;
+    if (container === null || fileURL === null) return;
     const wavesurfer = WaveSurfer.create({
       container: container,
-      url: fileURL!,
+      url: fileURL,
       plugins: [
         Hover.create({
           lineColor: "#ff0000",
@@ -23,7 +27,18 @@ function MusicTesting() {
         }),
       ],
     });
-  }, []);
+    const wsRegions = wavesurfer.registerPlugin(RegionsPlugin.create());
+
+    wavesurfer.on("decode", () => {
+      wsRegions.addRegion({
+        start: startTime,
+        end: endTime,
+        content: "Resize me",
+        drag: false,
+        resize: true,
+      });
+    });
+  }, [file]);
 
   function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.files) {
@@ -32,17 +47,19 @@ function MusicTesting() {
     }
   }
 
-  function submit(e: React.MouseEvent<HTMLButtonElement>) {
+  function refresh(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
+    setFlag(true);
   }
   return (
     <form>
       <div>
         <input type="file" accept="audio/*" onChange={handleFileChange} />
-        <button className="btn" onClick={submit}>
-          上傳
+        <button className="btn" onClick={refresh}>
+          重新上傳
         </button>
       </div>
+
       <div ref={containerRef}></div>
     </form>
   );
