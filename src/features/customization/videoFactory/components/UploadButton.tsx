dbc35ba8584c1991c771055526f70f4fbc2e8d2b
Chasel <arko9988b@gmail.com>
2024-06-02 16:48:59 +0800

    add waveSurfer to video container

--- src/features/customization/videoFactory/components/UploadButton.tsx
+++ src/features/customization/videoFactory/components/UploadButton.tsx
@@ -2,22 +2,37 @@ import { useRef } from "react";
 
 type UploadButtonProps = {
   hasVideo: boolean;
-  // changeVideo: () => void;
+  updateFile: (file: File) => void;
 };
 
 export function UploadButton(props: UploadButtonProps) {
-  const { hasVideo } = props;
+  const { hasVideo, updateFile } = props;
   const dialogRef = useRef<HTMLDialogElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
+  const buttonText = hasVideo ? "重新上傳影片" : "上傳影片";
+
+  const toggleUpload = () => {
+    if (hasVideo) {
+      handleOpenDialog();
+    } else {
+      handleUpload();
+    }
+  };
+
+  const handleOpenDialog = () => {
+    dialogRef.current?.showModal();
+  };
+
+  const handleUpload = () => {
+    fileInputRef.current?.click();
+    dialogRef.current?.close();
+  };
+
   return (
     <>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={() => fileInputRef.current?.click()}
-      >
-        {hasVideo ? "重新上傳影片" : "上傳影片"}
+      <button type="button" className="btn btn-primary" onClick={toggleUpload}>
+        {buttonText}
       </button>
 
       <input
@@ -25,20 +40,26 @@ export function UploadButton(props: UploadButtonProps) {
         accept="video/*"
         className="hidden"
         ref={fileInputRef}
-        onChange={(e) => console.log(e.target.files)}
+        onChange={(e) => {
+          if (e.target.files?.length) {
+            updateFile(e.target.files[0]);
+          }
+        }}
       />
 
       <dialog ref={dialogRef} className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">確定重新上傳圖片？</h3>
-          <p className="py-4">重新上傳圖片將會清除所有的修改</p>
+          <h3 className="font-bold text-lg">確定重新上傳影片？</h3>
+          <p className="py-4">重新上傳影片將會清除所有的修改</p>
           <div className="modal-action">
             <form method="dialog" className="space-x-3">
               <button className="btn btn-primary" type="submit">
                 返回
               </button>
             </form>
-            <button className="btn btn-primary">確定</button>
+            <button className="btn btn-primary" onClick={handleUpload}>
+              確定
+            </button>
           </div>
         </div>
       </dialog>
--- src/features/customization/videoFactory/components/VideoFactoryContainer.tsx
+++ src/features/customization/videoFactory/components/VideoFactoryContainer.tsx
@@ -1,5 +1,7 @@
 import { CustomizationDetail } from "@/types/Customization/CustomizationBase";
 import { UploadButton } from "./UploadButton";
+import WaveSurfer from "wavesurfer.js";
+import { useEffect, useRef, useState } from "react";
 
 type VideoFactoryContainerProps = {
   factoryData: CustomizationDetail;
@@ -8,13 +10,49 @@ type VideoFactoryContainerProps = {
 export function VideoFactoryContainer({
   factoryData,
 }: VideoFactoryContainerProps) {
-  console.log(factoryData);
+  const [file, setFile] = useState<File | null>(null);
+  const videoSrc = file ? URL.createObjectURL(file) : undefined;
+  const waveformRef = useRef<HTMLDivElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const updateFile = (file: File) => {
+    setFile(file);
+    if (videoSrc) URL.revokeObjectURL(videoSrc);
+  };
+
+  useEffect(() => {
+    if (!waveformRef.current || !videoRef.current || !file) return;
+
+    const ws = WaveSurfer.create({
+      container: waveformRef.current,
+      waveColor: "#263238",
+      progressColor: "rgb(100, 0, 100)",
+      media: videoRef.current,
+    });
+
+    return () => ws.destroy();
+  }, [file]);
+
   return (
     <>
       <div className="flex justify-end">
-        <UploadButton hasVideo={false} />
+        <UploadButton
+          hasVideo={Boolean(file)}
+          updateFile={(targetFile) => updateFile(targetFile)}
+        />
       </div>
-      <div>b</div>
+      <div className="flex flex-col items-center">
+        {videoSrc && (
+          <video
+            controls
+            playsInline
+            className="block"
+            ref={videoRef}
+            src={videoSrc}
+          />
+        )}
+        <div ref={waveformRef} className="w-full" />
+      </div>
     </>
   );
 }
